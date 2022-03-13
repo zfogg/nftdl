c809815b7b9c24a0be36665a09b1eb6be0399005
Zachary Fogg <me@zfo.gg>
2022-03-12 20:47:31 -0500

    cleanup ig idk

--- main.py
+++ main.py
@@ -20,20 +20,20 @@ def download_nft_json_and_img(root_dir, index, url):
     json_req = requests.get(url)
     json_blob = json_req.json()
     img_req = requests.get(json_blob['image'])
-    with open("{}/json/{:d}.json".format(root_dir, index), "wb") as file:
+    with open(f"{root_dir}/json/{index}.json", "wb") as file:
         file.write(json_req.content)
-    with open("{}/img/{:d}.png".format(root_dir, index), "wb") as file:
+    with open(f"{root_dir}/img/{index}.png", "wb") as file:
         file.write(img_req.content)
 
 
 def main():
     """download all the json and images for an nft given a url"""
     pwd = sys.path[0]
-    mkdir_fosho("{}/json".format(pwd))
-    mkdir_fosho("{}/img".format(pwd))
+    mkdir_fosho(f"{pwd}/json")
+    mkdir_fosho(f"{pwd}/img")
     # for index in range(1, 5001):
     for i in range(1, 10001):
-        url = "{}/{}".format(BASE_URL, i)
+        url = f"{BASE_URL}/{i}"
         pp.pprint((i, url))
         download_nft_json_and_img(pwd, i, url)
 
